83766750dcb7c110eeb9261f1f2617270ddcc7e6
Engreyxien <engreyxien@gmail.com>
2024-07-19 11:05:04 +0800

    api baseUrl was change

--- src/utilities/http.js
+++ src/utilities/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function useApi(token = null) {
   return axios.create({
-    baseURL: "https://el-elohe-production.onrender.com/api",
+    baseURL: "https://db-eebbqproduction.onrender.com/api",
     headers: {
       Accept: "application/json",
       Authorization: `Bearer ${token}`,
